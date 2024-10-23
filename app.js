e313c017734497d109d5d884520ae9ed2d3e9c56
mhanzl1 <105687913+mhanzl1@users.noreply.github.com>
2024-10-22 20:22:09 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+const navbarLinks = document.querySelectorAll('nav a');
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href');
+    document.querySelector(targetId).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+const myTags = [
+  'JavaScript', 'CSS', 'HTML',
+  'C', 'C++', 'React',
+  'Python', 'Java', 'git',
+  'django', 'Node.js', 'OpenCV',
+  'GCP', 'MySQL', 'jQuery',
+];
+
+var tagCloud = TagCloud('.content', myTags,{
+
+// radius in px
+radius: 250,
+
+// animation speed
+// slow, normal, fast
+maxSpeed: 'medium',
+initSpeed: 'medium',
+
+// 0 = top
+// 90 = left
+// 135 = right-bottom
+direction: 135,
+
+// interact with cursor move on mouse out
+keep: true
+
+});
+
+//To change the color of text randomly
+var colors = ['#00fdf1', '#d004f8ss'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
+
+
+
+
